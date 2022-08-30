890860bb7d3260a070546b900956f50cc97bf51c
Abdurrahman <abdoarafh54@gmail.com>
2022-08-30 13:21:30 +0200

    added: initial design of details section

---
+++ src/components/Details/Accordion.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect, useRef } from "preact/hooks";
+
+// assets
+import chevron from "/assets/chevron.svg";
+
+const Accordion = ({ name, num, open = false, children }) => {
+  const [isClosed, setIsClosed] = useState(false);
+  const [height, setHeight] = useState();
+  const bodyRef = useRef(null);
+  useEffect(() => {
+    if (bodyRef) {
+      const styles = getComputedStyle(bodyRef.current);
+      setHeight(styles.height);
+      setIsClosed(!open);
+    }
+  }, []);
+  return (
+    <div class="bg-black-gray text-white rounded-2xl my-6">
+      <div
+        class="px-12 py-6 flex justify-between items-center cursor-pointer hover:brightness-75 active:brightness-95"
+        onClick={() => setIsClosed((c) => !c)}
+      >
+        <div class="flex gap-4 items-center">
+          <h3 class="text-xl">{name}</h3>
+          <span class="bg-white/10 py-2 px-1 rounded-lg">{num}</span>
+        </div>
+        <button
+          style={{
+            transform: isClosed ? "rotate(180deg)" : "",
+            transition: "transform 0.35s ease",
+          }}
+        >
+          <img src={chevron} alt="..." />
+        </button>
+      </div>
+      <div
+        style={{
+          height: isClosed ? "0px" : height,
+          transition: "height 0.35s ease",
+        }}
+        class="px-12"
+        ref={bodyRef}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
